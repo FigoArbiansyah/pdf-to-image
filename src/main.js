3fef21107faa8c7c5bbd57f4c14e0646020f2245
Figo Arbiansyah <figoarbnsyh@gmail.com>
2026-07-22 18:56:04 +0700

    support analytic on vercel

--- src/main.js
+++ src/main.js
@@ -1,3 +1,8 @@
+import { inject } from '@vercel/analytics';
+
+// Initialize Vercel Analytics tracking
+inject();
+
 // Application State
 const state = {
   inputMode: 'none', // 'none' | 'pdf' | 'images'
